0f83f869dec72beb4b7ed16cbb6feb9747bbad3f
ignas13 <ltuwis@gmail.com>
2023-03-31 18:29:07 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+//pub mod reader;
+//pub mod line_parser;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+// asssemberis
+// kas turi but padaryta
+//
+// moduliai:
+// DONE     - skaityti faila -> output tekstinis blokas - string[]
+//      - parsinti kiekviena eilute, ignoruojant komentarus/whitespace -> string[]
+//      - nuspresti kokia tai komanda, A ar C -> grazinti struck kuris turetu initial komandas A ir C
+//      - parsinti kiekviena komanda is eiles A arba C
+//      - TODO: simboliai, linkai
+
+/*
+ * reikia perskaityiti kiekvina eilute
+ * - ignoruoti empty lines
+ * - ignoruoti komentarus
+ *
+ *
+ * kiekviena eilute isparsinti pagal komandos tipa:
+ * - prasideda @:
+ * -- A instrukcija: 
+ *      pasiimti kas eina po @ ir paversti i binary
+ *
+ * -- NEprasideda @:
+ * -- C instrucija:
+ *      isskaidyti kiekviena dali atskirai i jos komponentus TODO
+ */
+
+
+
