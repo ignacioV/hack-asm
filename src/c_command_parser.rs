d37a79d96952a632d6de92fa57d2b228696b6ac9
ignas13 <ltuwis@gmail.com>
2023-08-22 22:26:24 +0300

    Implement COMP creation from command

--- src/c_command_parser.rs
+++ src/c_command_parser.rs
@@ -9,19 +9,32 @@ pub struct C_CMD {
 pub enum COMP {
     _0,
     _1,
+    A
 }
 impl COMP {
+
+    pub fn from(comp_command: &str) -> Self {
+        match comp_command {
+            "0" => COMP::_0,
+            "1" => COMP::_1,
+            "A" => COMP::A,
+             _ => todo!("[{}] was not expected", comp_command),
+        }
+    }
+
     pub fn value(&self) -> String {
-        match self {
+        match &self {
             COMP::_0 => String::from("101010"),
             COMP::_1 => String::from("111111"),
+            COMP::A => String::from("110000"),
         }
     }
 
     pub fn a_val(&self) -> String {
-        match self {
+        match &self {
             COMP::_0 => String::from("0"),
             COMP::_1 => String::from("0"),
+            COMP::A => String::from("0"),
         }
     }
 }
@@ -46,6 +59,7 @@ impl JMP {
             JMP::null => String::from("000"),
         }
     }
+
 }
 
 
@@ -54,8 +68,11 @@ impl C_CMD {
     const FILLER: &str = "11";
 
     pub fn new(cmd: &str) -> Self {
+        // parse just COMP but how to determine
+        // jeigu yra = -> yea DEST -> nuo 0 iki `=` simbolio idx
+        // jeigu yra ; -> yra JMP -> nuo `;` simbolio idx iki pabaigos
         Self {
-            computation: COMP::_0,
+            computation: COMP::from(cmd),
             destination: DEST::null,
             jump: JMP::null,
         }
@@ -98,14 +115,26 @@ mod test_struct {
     #[test]
     fn should_generate_sample_c_command() {
         //given
-        let c_command: C_CMD = C_CMD::new("any");
+        let c_command: C_CMD = C_CMD::new("0");
 
         //when
         let result: String = c_command.to_binary();
 
         //then
         let expected: String = String::from("1110101010000000");
-        println!("{}", result);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn should_generate_c_command_from_string() {
+        //given
+        let c_command: C_CMD = C_CMD::new("A");
+
+        //when
+        let result: String = c_command.to_binary();
+
+        //then
+        let expected: String = String::from("1110110000000000");
         assert_eq!(result, expected);
     }
 }
